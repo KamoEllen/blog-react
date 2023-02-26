830fe16eb563734fd872830a2c502e3367614a7c
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2023-02-26 12:41:51 +0200

    Update index.jsx

--- blog/src/components/SingleArticle/index.jsx
+++ blog/src/components/SingleArticle/index.jsx
@@ -4,7 +4,11 @@ const SingleArticle = () => {
   return (
    <div>
   {/* Header */}
-  <header className="header header-inverse h-fullscreen pb-80" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/img/bg-cup.jpg)`}} data-overlay={8}>
+ <header className="header header-inverse h-fullscreen pb-80" data-overlay={8}>
+  <a href="https://data.whicdn.com/images/337152228/original.jpg">
+    <img src={`${process.env.PUBLIC_URL}/assets/img/bg-cup.jpg`} alt="Header background image" />
+  </a>
+
     <div className="container text-center">
       <div className="row h-full">
         <div className="col-12 col-lg-8 offset-lg-2 align-self-center">
@@ -19,7 +23,8 @@ const SingleArticle = () => {
             <a className="text-white" href="/">Hossein Shams</a>
           </p>
           <p>
-            <img className="rounded-circle w-40" src={`${process.env.PUBLIC_URL}/assets/img/avatar/2.jpg`} alt="..." />
+            <img className="rounded-circle w-40" src="https://data.whicdn.com/images/337152228/original.jpg" alt="...">
+
           </p>
         </div>
         <div className="col-12 align-self-end text-center">
@@ -38,73 +43,48 @@ const SingleArticle = () => {
 | Blog content
 |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
 !*/}
-    <div className="section" id="section-content">
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-lg-8 offset-lg-2">
-            <p className="lead">Dynamically procrastinate professional best practices vis-a-vis next-generation content. Seamlessly extend sustainable
-              networks via plug-and-play niche markets. Progressively communicate customized alignments for client-focused
-              services. Holisticly cultivate professional mindshare with impactful niche markets. Dynamically fabricate market-driven
-              collaboration and idea-sharing with turnkey.</p>
-            <hr className="w-100" />
-            <p>Proactively whiteboard covalent functionalities through reliable applications. Professionally integrate 24/365
-              manufactured products rather than end-to-end collaboration and idea-sharing. Conveniently disintermediate progressive
-              scenarios via inexpensive ROI. Proactively build seamless markets rather than world-class channels. Objectively
-              disseminate multimedia based methodologies whereas robust.</p>
-            <p>Compellingly plagiarize economically sound opportunities and cross-platform e-business. Completely embrace alternative
-              initiatives whereas innovative services. Holisticly e-enable one-to-one internal or "organic" sources via interactive
-              technologies. Energistically leverage other's end-to-end e-tailers with goal-oriented innovation. Objectively
-              evolve focused networks for unique methods.</p>
-          </div>
-        </div>
-        <br />
-        <p>
-          <img src={`${process.env.PUBLIC_URL}/assets/img/blog-img.jpg`} alt="..." />
-        </p>
-        <br />
-        <div className="row">
-          <div className="col-12 col-lg-8 offset-lg-2">
-            <p>Assertively empower customized alignments vis-a-vis go forward vortals. Completely monetize inexpensive alignments
-              whereas market positioning services. Uniquely revolutionize multifunctional strategic theme areas with adaptive
-              architectures. Interactively orchestrate next-generation content with proactive applications. Appropriately
-              initiate market-driven results through backend deliverables.</p>
-            <ul>
-              <li>Energistically coordinate business relationships</li>
-              <li>Change via resource-leveling innovation</li>
-              <li>Efficiently develop fully researched experiences and integrated</li>
-              <li>Assertively empower customized alignments vis-a-vis go forward vortals</li>
-            </ul>
-            <p>Quickly innovate high-quality catalysts for change via resource-leveling innovation. Seamlessly expedite just
-              in time infomediaries vis-a-vis viral manufactured products. Rapidiously network synergistic leadership whereas
-              emerging data. Professionally synergize compelling partnerships vis-a-vis leading-edge e-business. Efficiently
-              develop fully researched experiences and integrated. Phosfluorescently simplify ubiquitous bandwidth without
-              client-centered information. Professionally recaptiualize flexible relationships via performance based synergy.
-              Monotonectally mesh interoperable processes through high-quality e-commerce. Dynamically build team building
-              models without 24/7 internal or "organic" sources. Compellingly incentivize e-business methods of empowerment
-              with.
-            </p>
-            <blockquote className="blockquote">
-              <div className="quote-sign" />
-              <p>Objectively grow out-of-the-box results with go forward methodologies. Progressively exploit dynamic functionalities
-                without ethical portals. Competently facilitate timely total linkage with.</p>
-              <footer>Someone famous in
-                <cite title="Source Title">Source Title</cite>
-              </footer>
-            </blockquote>
-            <p>Proactively underwhelm proactive leadership skills whereas plug-and-play paradigms. Credibly whiteboard client-centered
-              intellectual capital after B2B total linkage. Progressively innovate technically sound manufactured products
-              whereas diverse meta-services. Continually orchestrate ubiquitous core competencies rather than market positioning
-              "outside the box" thinking. Dramatically redefine out-of-the-box schemas.</p>
-            <div className="gap-multiline-items-1 mt-30">
-              <a className="badge badge-pill badge-default" href="/">Record</a>
-              <a className="badge badge-pill badge-default" href="/">Progress</a>
-              <a className="badge badge-pill badge-default" href="/">Customers</a>
-              <a className="badge badge-pill badge-default" href="/">News</a>
-            </div>
-          </div>
+<div className="section" id="section-content">
+  <div className="container">
+    <div className="row">
+      <div className="col-12 col-lg-8 offset-lg-2">
+        <p className="lead">Discover how you can make money online by leveraging the power of next-generation content and sustainable networks. Learn to customize your services to attract more clients and build a professional reputation in your niche market. </p>
+        <hr className="w-100" />
+        <p>Proactively build your brand's online presence through reliable applications and seamless markets. Don't wait any longer to monetize your skills and expertise. </p>
+        <p>Compellingly embrace innovative services and alternative initiatives to tap into new revenue streams. Utilize interactive technologies to grow your business and attract more customers. </p>
+      </div>
+    </div>
+    <br />
+    <p>
+      <img src="https://data.whicdn.com/images/337152228/original.jpg" alt="...">
+    </p>
+    <br />
+    <div className="row">
+      <div className="col-12 col-lg-8 offset-lg-2">
+        <p>Assertively empower your business with customized alignments and market positioning services. Revolutionize your approach to strategic theme areas and leverage adaptive architectures to stay ahead of the competition. </p>
+        <ul>
+          <li>Coordinate business relationships to maximize profits</li>
+          <li>Develop fully researched experiences and integrated systems</li>
+          <li>Incentivize e-business methods of empowerment</li>
+          <li>Empower customized alignments with go-forward vortals</li>
+        </ul>
+        <p>Quickly innovate high-quality catalysts for change and expedite infomediaries to promote your products. Network with other leaders in your industry to stay up-to-date on emerging trends and data. </p>
+        <blockquote className="blockquote">
+          <div className="quote-sign" />
+          <p>Objectively grow out-of-the-box results with go-forward methodologies. Progressively exploit dynamic functionalities to drive profits. Competently facilitate timely total linkage with your customers.</p>
+          <footer>Someone famous in
+            <cite title="Source Title">Source Title</cite>
+          </footer>
+        </blockquote>
+        <p>Proactively develop your leadership skills and plug-and-play paradigms to scale your business. Build client-centered intellectual capital after B2B total linkage to create a sustainable revenue stream. Continually innovate and redefine out-of-the-box schemas to stay ahead of the curve. </p>
+        <div className="gap-multiline-items-1 mt-30">
+          <a className="badge badge-pill badge-default" href="/">Record your progress</a>
+          <a className="badge badge-pill badge-default" href="/">Track your customers</a>
+          <a className="badge badge-pill badge-default" href="/">Stay up-to-date with news</a>
         </div>
       </div>
     </div>
+  </div>
+</div> 
     {/*
 |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
 | Comments
