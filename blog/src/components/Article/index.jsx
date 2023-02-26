ae110a7a0c51b898760ba7a6588cabd5c4bc7fcb
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2023-02-26 12:08:13 +0200

    Update index.jsx

--- blog/src/components/Article/index.jsx
+++ blog/src/components/Article/index.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './Article.css';
 
 const Article = () => {
   return (
-    <article className="mt-90">
-  <header className="text-center mb-40">
-    <h3>
-      <a href="blog-single.html">New features will add to dashboard soon</a>
-    </h3>
-    <div className="link-color-default fs-12">
-      <a href="/">News</a>,
-      <time>May 13, 2017</time>
+    <div className="article-page">
+      <h1 className="article-title">Making Money with React</h1>
+      <p className="article-summary">React is a popular JavaScript library used for building dynamic user interfaces. With its ability to create reusable components and handle complex state management, it has become a powerful tool for building web applications and sites. But did you know that you can also use React to make money?</p>
+      <div className="article-content">
+        <h2>1. Creating Web Applications</h2>
+        <p>One of the most common ways to make money with React is by building web applications for clients. You can create custom web applications that solve specific problems for businesses or individuals, and charge them for your services. With React, you can create dynamic user interfaces that are fast and responsive, making for a great user experience.</p>
+        <h2>2. Selling Templates and Components</h2>
+        <p>If you're an experienced React developer, you can also create and sell templates and components. These are pre-built pieces of code that can be used by other developers to create their own web applications. There are many marketplaces, such as ThemeForest and CodeCanyon, where you can sell your templates and components. This can be a great way to earn passive income, as you can continue to make money off of your creations long after they're built.</p>
+        <h2>3. Teaching React</h2>
+        <p>Another way to make money with React is by teaching it. If you're an expert in React, you can create online courses or tutorials to teach others how to use the library. Platforms like Udemy and Skillshare allow you to create and sell your courses to a wide audience. This can be a great way to make money while sharing your knowledge with others.</p>
+        <h2>4. Building and Selling Your Own Products</h2>
+        <p>If you have an idea for a product, you can use React to build it. Whether it's a web application or a mobile app, React can help you create a fast and responsive user interface. Once you've built your product, you can sell it through your own website or through marketplaces like the Apple App Store or Google Play Store.</p>
+      </div>
     </div>
-  </header>
-  <a href="blog-single.html">
-    <img className="rounded" src="assets/img/blog-1.jpg" alt="..." />
-  </a>
-  <div className="card-block">
-    <p className="text-justify">Together. Great. So good was saying, that can't first let called air divide stars male isn't i. Herb third let
-      may fourth divide. Greater gathering land you'll i their beast have. She'd form sea it wherein fowl, spirit
-      creeping living. Likeness creepeth you hath heaven. Likeness, moveth fruitful behold. Open evening a air us
-      behold. Saying above moving second a subdue likeness after also second.</p>
-    <p className="text-center mt-40">
-      <Link className="btn btn-primary btn-round" to="/article/some-title-slug">Read more</Link>
-    </p>
-  </div>
-</article>
-
   );
 };
 
